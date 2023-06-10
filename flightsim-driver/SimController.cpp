1ce8e4e1bf1294f5fee8ab8379acfcccc8d17ae5
Mateusz Chwast <mateusz.chwast@revolmatic.com>
2023-06-10 12:58:51 +0200

    implement connecting and disconnecting with simconnect

--- flightsim-driver/SimController.cpp
+++ flightsim-driver/SimController.cpp
@@ -1,1 +1,44 @@
+#include <iostream>
 #include "SimController.h"
+
+bool SimController::TryConnect()
+{
+    std::cout << "Connecting...\r\n";
+
+    HRESULT result;
+    result = SimConnect_Open(&hSimConnect, "a320 hardware driver", NULL, 0, 0, 0);
+
+    _isConnected = false;
+
+    if (result != S_OK)
+    {
+        std::cout << "Unable to connect.\r\n";
+        hSimConnect = nullptr;
+        return false;
+    }
+
+    _isConnected = true;
+    std::cout << "Connected!\r\n";
+    return true;
+}
+
+void SimController::Disconnect()
+{
+    std::cout << "Disconnecting...\r\n";
+    if (hSimConnect == nullptr || _isConnected == false)
+    {
+        std::cout << "Already disconnected\r\n";
+        return;
+    }
+
+    _isConnected = false;
+    HRESULT res = SimConnect_Close(hSimConnect);
+    
+    if (res == S_OK)
+    {
+        std::cout << "Disconnected.\r\n";
+        return;
+    }
+
+    std::cout << "Disconnection error\r\n";
+}
--- flightsim-driver/SimController.h
+++ flightsim-driver/SimController.h
@@ -1,7 +1,18 @@
 #pragma once
 
+#include <windows.h>
+#include "SimConnect.h"
+
 class SimController
 {
+private:
+	HANDLE hSimConnect = nullptr;
+	bool _isConnected = false;
+
+	bool TryConnect();
+	void Disconnect();
+
+public:
 
 };
 
