2bbb699d0a96e05c6e8b1cd988fada40a746aea6
Mateusz Chwast <mateusz.chwast@revolmatic.com>
2023-06-10 13:44:51 +0200

    implement controller thread

--- flightsim-driver/SimController.cpp
+++ flightsim-driver/SimController.cpp
@@ -2,19 +2,36 @@
 #include <chrono>
 #include "SimController.h"
 
+void SimController::ControllerHandler()
+{
+    while (_stopControllerFlag == false)
+    {
+        if (_startSearchFlag == true)
+        {
+            _startSearchFlag = false;
+            StartSearch(1000);
+        }
+        if (_stopSearchFlag == true)
+        {
+            _stopSearchFlag = false;
+            StopSearch();
+        }
+    }
+}
+
 bool SimController::TryConnect()
 {
     std::cout << "Connecting...\r\n";
 
     HRESULT result;
-    result = SimConnect_Open(&hSimConnect, "a320 hardware driver", NULL, 0, 0, 0);
+    result = SimConnect_Open(&_hSimConnect, "a320 hardware driver", NULL, 0, 0, 0);
 
     _isConnected = false;
 
     if (result != S_OK)
     {
         std::cout << "Unable to connect.\r\n";
-        hSimConnect = nullptr;
+        _hSimConnect = nullptr;
         return false;
     }
 
@@ -26,14 +43,14 @@ bool SimController::TryConnect()
 void SimController::Disconnect()
 {
     std::cout << "Disconnecting...\r\n";
-    if (hSimConnect == nullptr || _isConnected == false)
+    if (_hSimConnect == nullptr || _isConnected == false)
     {
         std::cout << "Already disconnected\r\n";
         return;
     }
 
     _isConnected = false;
-    HRESULT res = SimConnect_Close(hSimConnect);
+    HRESULT res = SimConnect_Close(_hSimConnect);
     
     if (res == S_OK)
     {
@@ -62,7 +79,22 @@ void SimController::TryConnectPeriodically(unsigned period, bool connectOnce)
     }
 }
 
-void SimController::SearchForServer(unsigned period, bool stopSearchAfterFirstConnection)
+SimController::SimController()
+{
+    _controllerThread = new std::thread(&SimController::ControllerHandler, this);
+}
+
+void SimController::SearchForServer()
+{
+    _startSearchFlag = true;
+}
+
+void SimController::StopSearchForServer()
+{
+    _stopSearchFlag = true;
+}
+
+void SimController::StartSearch(unsigned period, bool stopSearchAfterFirstConnection)
 {
     //something went wrong and search was called twice, so stop the previous instance
     if (_searchThread != nullptr)
@@ -70,7 +102,7 @@ void SimController::SearchForServer(unsigned period, bool stopSearchAfterFirstCo
 
     std::cout << "Searching for server...\r\n";
     _searchActive = true;
-    _searchThread = new std::thread(&SimController::TryConnectPeriodically, 
+    _searchThread = new std::thread(&SimController::TryConnectPeriodically,
         this, period, stopSearchAfterFirstConnection);
 }
 
@@ -87,4 +119,14 @@ void SimController::StopSearch()
 
     delete _searchThread;
     _searchThread = nullptr;
+}
+
+SimController::~SimController()
+{
+    if (_controllerThread == nullptr)
+        return;
+
+    _controllerThread->join();
+    delete _controllerThread;
+    _controllerThread = nullptr;
 }
--- flightsim-driver/SimController.h
+++ flightsim-driver/SimController.h
@@ -8,21 +8,31 @@ class SimController
 {
 private:
 	//thread that runs all controller logic
-	std::thread* controllerThread = nullptr;
+	std::thread* _controllerThread = nullptr;
+	bool _stopSearchFlag = false;
+	bool _startSearchFlag = false;
+	bool _stopControllerFlag = false;
 
-	HANDLE hSimConnect = nullptr;
+	HANDLE _hSimConnect = nullptr;
 	bool _isConnected = false;
 
 	//thread that runs only server search
 	std::thread* _searchThread = nullptr;
 	volatile bool _searchActive = false;
 
+	void ControllerHandler();
 	bool TryConnect();
 	void Disconnect();
 	void TryConnectPeriodically(unsigned period, bool connectOnce);
 
-public:
-	void SearchForServer(unsigned period, bool stopSearchAfterFirstConnection = false);
+	void StartSearch(unsigned period, bool stopSearchAfterFirstConnection = false);
 	void StopSearch();
+public:
+	SimController();
+
+	void SearchForServer();
+	void StopSearchForServer();
+
+	~SimController();
 };
 
