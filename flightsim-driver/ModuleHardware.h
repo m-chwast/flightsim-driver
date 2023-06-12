fa2d2633ee18f1833a2cbeeaed62312e3b47abf2
Mateusz Chwast <mateusz.chwast@revolmatic.com>
2023-06-12 16:15:46 +0200

    add module hardware and control device classes

---
+++ flightsim-driver/ModuleHardware.h
@@ -0,0 +1,11 @@
+#pragma once
+#include "ModuleBase.h"
+#include "ControlDevice.h"
+#include <vector>
+
+class ModuleHardware : public ModuleBase
+{
+private:
+	std::vector<ControlDevice> _devices;
+};
+
