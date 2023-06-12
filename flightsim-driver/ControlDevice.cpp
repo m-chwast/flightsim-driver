f7a0ccafe5d9d97a5014270aba2a5786a39c7946
Mateusz Chwast <mateusz.chwast@revolmatic.com>
2023-06-12 16:42:48 +0200

    add basic functions and fields to control device class

--- flightsim-driver/ControlDevice.cpp
+++ flightsim-driver/ControlDevice.cpp
@@ -1,1 +1,12 @@
 #include "ControlDevice.h"
+#include <iostream>
+
+void ControlDevice::EventHandler(int eventID, int data)
+{
+	std::cout << "Default event handler called for the device: " << _name << "\r\n";
+}
+
+void ControlDevice::DataHandler(const uint8_t* data)
+{
+	std::cout << "Default data handler called for the device: " << _name << "\r\n";
+}
--- flightsim-driver/ControlDevice.h
+++ flightsim-driver/ControlDevice.h
@@ -1,7 +1,36 @@
 #pragma once
 
+#include <vector>
+#include <string>
+
 class ControlDevice
 {
+private:
+	unsigned _id;
+	std::string _name;
+	std::vector<std::string> _eventNames;
+	std::vector<std::string> _dataNames;
 
+public:
+	ControlDevice(unsigned id, const char * name,
+		std::vector<std::string> eventNames, std::vector<std::string> dataNames)
+	{
+		_id = id;
+		_eventNames = eventNames;
+		_dataNames = dataNames;
+		_name = name;
+	}
+
+	/// <summary>
+	/// called when any of the device's event is received
+	/// </summary>
+	virtual void EventHandler(int eventID, int data);
+	
+	/// <summary>
+	/// called when data requested by the device is provided 
+	/// </summary>
+	virtual void DataHandler(const uint8_t * data);
+
+	virtual ~ControlDevice() {}
 };
 
