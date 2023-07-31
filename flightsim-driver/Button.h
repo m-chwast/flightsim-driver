ab26aaecc1cb4f155db9ad54c4bd696a459304af
Mateusz Chwast <chwastmat@gmail.com>
2023-08-01 01:33:26 +0200

    add error msg in press method

--- flightsim-driver/Button.h
+++ flightsim-driver/Button.h
@@ -1,5 +1,6 @@
 #pragma once
 #include <string>
+#include <exception>
 #include "SimServices.h"
 #include "ConsoleManager.h"
 
@@ -18,11 +19,21 @@ public:
 
 	Button(unsigned moduleID, unsigned eventID, const char* eventName, unsigned dataRequestID, const SimServices* simServices, ConsoleManager* console)
 		: _moduleID{ moduleID }, _eventID{ eventID }, _eventName{ eventName }, _dataRequestID{ dataRequestID }, _simServices{ simServices }, _console{ console }
-	{}
+	{
+		if (_simServices == nullptr || _console == nullptr)
+			throw std::exception("simServices and console can't be null!");
+	}
 
 	virtual void Press() const
 	{
-		_simServices->InvokeSimEvent(_moduleID, _eventID);
-		_simServices->RequestData(_moduleID, _dataRequestID);
+		bool eventOk = _simServices->InvokeSimEvent(_moduleID, _eventID);
+		bool dataRequestOk = _simServices->RequestData(_moduleID, _dataRequestID);
+
+		if (eventOk == false || dataRequestOk == false)
+		{
+			std::string msg = "ERROR: Module " + std::to_string(_moduleID);
+			msg += ", Event " + std::to_string(_eventID) + ": Press failed\r\n";
+			_console->Send(msg);
+		}
 	}
 };
