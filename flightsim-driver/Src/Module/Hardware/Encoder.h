d9ae10cf52923592167f73c9612b815ef9379c23
Mateusz Chwast <chwastmat@gmail.com>
2023-11-18 16:09:27 +0100

    add inc and dec events to encoder

--- flightsim-driver/Src/Module/Hardware/Encoder.h
+++ flightsim-driver/Src/Module/Hardware/Encoder.h
@@ -4,14 +4,24 @@
 #include "SimServices.h"
 #include "ConsoleManager.h"
 #include "HardwareElementBase.h"
+#include "SimEvent.h"
 
 
 class Encoder : public HardwareElementBase
 {
 private:
+	const SimEvent _incEvent;
+	const SimEvent _decEvent;
 
 public:
-	Encoder(HardwareElementBase& base)
-		: HardwareElementBase(base)
+	Encoder(HardwareElementBase& base, SimEvent incEvent, SimEvent decEvent)
+		: HardwareElementBase(base), _incEvent{ incEvent }, _decEvent{ decEvent }
 	{}
+
+	virtual bool EventsSetup() const
+	{
+		bool setupOk = _simServices.SetUpSimEvent(_moduleID, _incEvent.GetID(), _incEvent.GetName());
+		setupOk |= _simServices.SetUpSimEvent(_moduleID, _decEvent.GetID(), _decEvent.GetName());
+		return setupOk;
+	}
 };
