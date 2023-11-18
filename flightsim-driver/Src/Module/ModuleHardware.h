887299756a47f791cd63b47111b996375df2708a
Mateusz Chwast <chwastmat@gmail.com>
2023-11-18 16:35:01 +0100

    make moduleHardware an interface

--- flightsim-driver/Src/Module/ModuleHardware.h
+++ flightsim-driver/Src/Module/ModuleHardware.h
@@ -4,24 +4,24 @@
 class ModuleHardware : public ModuleBase
 {
 private:
-	unsigned _id;
+	const unsigned _id;
 
 	virtual bool EventsInitialize() { return false; };
-	virtual bool DataInitialize() { return false; };
+	virtual bool DataInitialize() = 0;
 
 protected:
 	int _dataUpdatePeriod = -1;	//-1 -> no update, period is in ms 
 
 public:
-	ModuleHardware(const SimServices& simServices, ConsoleManager& console, unsigned id) : ModuleBase(simServices, console)
-	{
-		_id = id;
-	}
+	ModuleHardware(const SimServices& simServices, ConsoleManager& console, unsigned id)
+		: ModuleBase(simServices, console),
+		_id{ id }
+	{}
 
 	unsigned GetID() const { return _id; }
 
-	virtual void ProcessEvent(const SIMCONNECT_RECV_EVENT* data) {};
-	virtual void ProcessData(const SIMCONNECT_RECV_SIMOBJECT_DATA* data) {};
+	virtual void ProcessEvent(const SIMCONNECT_RECV_EVENT* data) = 0;
+	virtual void ProcessData(const SIMCONNECT_RECV_SIMOBJECT_DATA* data) = 0;
 
 	virtual void Initialize() override;
 	virtual void Manage() override;
