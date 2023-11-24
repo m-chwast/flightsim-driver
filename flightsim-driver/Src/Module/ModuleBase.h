ba9dd876b4e24225210106e6015cec429df33537
Mateusz Chwast <chwastmat@gmail.com>
2023-11-25 00:25:37 +0100

    store commManager reference in moduleBase

--- flightsim-driver/Src/Module/ModuleBase.h
+++ flightsim-driver/Src/Module/ModuleBase.h
@@ -1,8 +1,6 @@
 #pragma once
 
 #include <string>
-#include "SimServices.h"
-#include "ConsoleManager.h"
 #include "ModuleUtils.h"
 
 
@@ -12,12 +10,13 @@ protected:
 	std::string _name = "defaultName";
 	const SimServices&  _simServices;
 	ConsoleManager& _console;
+	Comm::CommManager& _commManager;
 
 	void PrintInitMessage();
 
 public:
 	ModuleBase(const ModuleUtils& utils)
-		: _simServices{utils.GetSimServices()}, _console{utils.GetConsole()}
+		: _simServices{utils.GetSimServices()}, _console{utils.GetConsole()}, _commManager{utils.GetCommManager()}
 	{}
 	
 	const char* GetName() const { return _name.c_str(); }
