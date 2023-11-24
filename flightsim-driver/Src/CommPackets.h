8cacf94195d0f0438daacdf452e265837cad1b6e
Mateusz Chwast <chwastmat@gmail.com>
2023-11-24 23:22:22 +0100

    create basic data packets

---
+++ flightsim-driver/Src/CommPackets.h
@@ -0,0 +1,29 @@
+#pragma once
+#include <cstdint>
+
+
+namespace Comm
+{
+	enum PacketHeader
+	{
+		COMM_PACKET_HEADER_STD,
+	};
+
+	enum PacketType
+	{
+		COMM_PACKET_TYPE_MODULE_DATA,
+	};
+
+	struct Packet
+	{
+		uint8_t header;
+		uint16_t size;
+		uint8_t type;
+		uint32_t crc;
+	};
+
+	struct ModuleDataPacket : Packet
+	{
+		uint8_t moduleID;
+	};
+}
